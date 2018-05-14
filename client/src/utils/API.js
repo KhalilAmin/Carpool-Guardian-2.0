336ee1c0d6679dc48f3ad8ee05f1639770c55d0a
Slickrick88 <rickablake@gmail.com>
2018-05-14 18:50:04 -0400

    updates to router and other shit

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,10 +17,66 @@ export default {
   },
 
   addFace: function(data) {
-    return axios.post("api/face/addFace", data)
+    return axios.post("/api/face/addFace", data)
   },
 
   searchFace: function(data) {
-    return axios.post("api/face/searchFace", data)
+    return axios.post("/api/face/searchFace", data)
   },
+  // get guadian tied to a face tocken (pulled from getNext)
+  // and on load of parent portal
+  getGaurdian: function (data) {
+    return axios.get("/api/db/getGaurdian" + data);
+  },
+  // Gets all chillren tied to a family (parent Portal) and 
+  // get all children tied to a specific Guardian on pickup for a specific cone 
+  getChildren: function (data) {
+    return axios.get("/api/db/getChildren" + data);
+  },
+  // Gets all teachers tied to a school
+  getTeachers: function (id) {
+    return axios.get("/api/db/getTeachers" + id);
+  },
+  // add a child to a family
+  addChild: function (childData) {
+    return axios.post("/api/db/addChild", childData);
+  },
+  addFamily: function (familyData) {
+    return axios.post("/api/db/addFamily", familyData);
+  },
+  //add a guardian to a family
+  addGaurdian: function (gaurdianData) {
+    return axios.post("/api/db/addGaurdian", gaurdianData);
+  },
+  //add temp gaudian to a family
+  addTemp: function (tempData) {
+    return axios.post("/api/db/addTemp", tempData);
+  },
+  // update guardian info
+  updateGaurdian: function (gaurdianData) {
+    return axios.post("/api/db/updateGaurdian", gaurdianData);
+  },
+  // update child info
+  updateChild: function (childData) {
+    return axios.post("/api/db/updateChild", childData);
+  },
+  //Update family data
+  updateFamily: function (familyData) {
+    return axios.post("/api/db/updateFamily", familyData);
+  },
+  //help
+  //updates cone Schema to add people to queue 
+  addQueue: function (face_Token, cone_id) {
+    return axios.post("/api/books", bookData);
+  },
+  //Removes people to queue on pickup
+  removeQueue: function (id) {
+    return axios.delete("/api/db/removeQueue", id);
+  },
+  //gets the next person in the queue for a specific cone
+  //uses face_Token
+  getNext: function (data) {
+    return axios.get("/api/db/getNext", data);
+  },  
+  
 };
--- routes/api/db.js
+++ routes/api/db.js
@@ -1,55 +1,43 @@
-import axios from "axios";
+// import axios from "axios";
+const router = require("express").Router();
+const dbController = require("../../controllers/dbController");
 
-export default {
-  // get guadian tied to a face tocken (pulled from getNext)
-  // and on load of parent portal
-  getGaurdian: function (face_token) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Gets all chillren tied to a family (parent Portal) and 
-  // get all children tied to a specific Guardian on pickup for a specific cone 
-  getChildren: function () {
-    return axios.get("/api/books");
-  },
-  // Gets all teachers tied to a school
-  getTeachers: function (id) {
-    return axios.get("/api/books/" + id);
-  },
-  // add a child to a family
-  addChild: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  //add a guardian to a family
-  addGaurdian: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  //add temp gaudian to a family
-  addTemp: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  // update guardian info
-  updateGaurdian: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  // update child info
-  updateChild: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  //Update family data
-  updateFamily: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  //updates cone Schema to add people to queue 
-  addQueue: function (face_Token, cone_id) {
-    return axios.post("/api/books", bookData);
-  },
-  //Removes people to queue on pickup
-  removeQueue: function (bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  //gets the next person in the queue for a specific cone
-  //uses face_Token
-  getNext: function (faceToken) {
-    return axios.post("/api/books", bookData);
-  },
-};
+
+router.route("/getGaurdian")
+  .post(dbController.getGaurdian);
+
+router.route("/getChildren")
+  .post(dbController.getChildren);
+
+router.route("/getTeachers")
+  .post(dbController.getTeachers);
+
+router.route("/addChild")
+  .post(dbController.addChild);
+
+router.route("/addGaurdian")
+  .post(dbController.addGaurdian);
+
+router.route("/addTemp")
+  .post(dbController.addTemp);
+
+router.route("/updateGaurdian")
+  .post(dbController.updateGaurdian);
+
+router.route("/updateChild")
+  .post(dbController.updateChild);
+
+router.route("/updateFamily")
+  .post(dbController.updateFamily);
+
+router.route("/addQueue")
+  .post(dbController.addQueue);
+
+router.route("/removeQueue")
+  .post(dbController.removeQueue);
+
+router.route("/getNext")
+  .get(dbController.getNext);
+
+
+module.exports = router;
