13a5af4796b97ff2e0740680c37b0a12d08288ed
khalil <kamin86@gmail.com>
2018-05-14 21:18:21 -0400

    ivelise imagecard

--- client/src/components/StuInfo/stuInfoCard.js
+++ client/src/components/StuInfo/stuInfoCard.js
@@ -2,25 +2,28 @@ import React from "react";
 
 const stuInfoCard = props => {
 
-  const personArray = props.personArray;
-  const list = personArray.map((person) =>
-    <li key={person.id}>  </li>
-    // <div className="">
-    //   <ul>
-    //     <li>
-    //       <strong>Name:</strong> {person.firstname}{person.lastname}
-    //     </li>
-    //     <li>
-    //       <strong>Occupation:</strong> {props.occupation}
-    //     </li>
-    //     <li>
-    //       <strong>Location:</strong> {props.location}
-    //     </li>
-    //   </ul>
-    // </div>
-  );
+    const personArray = props.personArray;
+    const list = personArray.map((person) =>
+        // <li key={person.id }>  </li>
+        <div className="">
+          <ul>
+            <li>
+              <strong>Name:</strong> {person.firstname}{person.lastname}
+            </li>
+            <li>
+              <strong>Email:</strong> {props.email}
+            </li>
+            <li>
+              <strong>Location:</strong> {props.location}
+            </li>
+            <li>
+              <strong>Phone:</strong> {props.phone}
+            </li>
+          </ul>
+        </div>
+    );
 
-  return (<ul>{list}</ul>);
+    return {list};
 }
 
 export default stuInfoCard;
