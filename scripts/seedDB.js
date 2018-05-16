d339beedbf833f295062bc0db0b1081a25fa4453
pawhalen <pgwhal@gmail.com>
2018-05-16 19:32:04 -0400

    not much change

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 const db = require("../models");
 mongoose.Promise = global.Promise;
 
-// This file empties the Books collection and inserts the books below
-
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/carpoolGaurdian",
+  process.env.MONGODB_URI || "mongodb://localhost/carpoolGuardian",
   {
     useMongoClient: true
   }
