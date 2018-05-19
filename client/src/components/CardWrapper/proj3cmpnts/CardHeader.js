c5bc9c544c7d22ca49a1f018769415c12fb676b7
Samuel P Taylor III <s.p.taylor.3@gmail.com>
2018-05-19 08:36:55 -0400

    image proto-card layout complete

---
+++ client/src/components/CardWrapper/proj3cmpnts/CardHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+
+class CardHeader extends Component {
+
+  render() {
+    return (
+      <strong><h1 style={{ textAlign: 'center', marginTop: '30px', fontSize: '55px' }}>Carpool Candidate</h1></strong>
+    )
+  }
+}
+
+export default CardHeader
