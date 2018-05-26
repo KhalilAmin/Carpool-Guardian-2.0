ebea34627e308ef52e7d1820a7ce2e907fc222cd
Trevor Harm <trevorharm@users.noreply.github.com>
2018-05-26 09:46:25 -0400

    lastDay1Push

--- client/src/components/PatsTempComponents/AddGuardCard/AddGuardCard.js
+++ client/src/components/PatsTempComponents/AddGuardCard/AddGuardCard.js
@@ -127,6 +127,7 @@ class AddGuardCard extends Component {
                                 value={this.state.password}
                                 onChange={this.handleInputChange}
                                 name="password"
+                                type="password"
                                 pattern="/\d{3}-\d{3}-\d{4}/"
                                 placeholder="Enter Password"
                             />
