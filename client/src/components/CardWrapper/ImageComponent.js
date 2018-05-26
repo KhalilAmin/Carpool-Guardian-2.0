6133cd3572793df42d45815fac3250aad9293c8f
pawhalen <pgwhal@gmail.com>
2018-05-26 07:21:45 -0400

    changes for deployment test

--- client/src/components/CardWrapper/ImageComponent.js
+++ client/src/components/CardWrapper/ImageComponent.js
@@ -4,6 +4,11 @@ import { relative } from 'path';
 
 class ImageComponent extends Component {
 
+
+
+
+
+
   render() {
     return (
       <div style={{ border: '3px solid' }}>
@@ -15,11 +20,6 @@ class ImageComponent extends Component {
               <h3>{this.props.image_heading}</h3>
               <img src={"data:image/png;base64," + this.props.img} alt={this.props.fName} style={{ width: '85%', height: '85%' }} />
               <h5>{this.props.name}</h5>
-
-              <div className='w3-section'>
-                <button className="w3-button w3-green" style={{ width: '30%', marginRight: '5%' }} >Accept </button>
-                <button className="w3-button w3-red" style={{ width: '30%' }} > Decline </button>
-              </div>
             </div>
 
           </div>
