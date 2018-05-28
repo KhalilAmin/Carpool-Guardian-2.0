fb0c2c41960996d0d321507eaffa51f364bb7a67
Ivelise Sola <solaivelise@yahoo.com>
2018-05-28 17:55:55 -0400

    AddChildCard form style

--- client/src/components/PatsTempComponents/AddChildCard/AddChildCard.js
+++ client/src/components/PatsTempComponents/AddChildCard/AddChildCard.js
@@ -4,6 +4,11 @@ import { Col, Row, Container } from "../../../components/Grid";
 import { Input, FormBtn } from "../../../components/Form";
 import AddImageCard from "../AddImageCard"
 import { EHOSTUNREACH } from "constants";
+import Grid from '@material-ui/core/Grid';
+import Person from '@material-ui/icons/Person';
+import Email from '@material-ui/icons/Email';
+import Phone from '@material-ui/icons/Phone';
+import LocalLibrary from '@material-ui/icons/LocalLibrary';
 
 
 class AddChildCard extends Component {
@@ -52,15 +57,15 @@ class AddChildCard extends Component {
         for (let i = 0; i < this.props.familyObject.guardian.length; i++) {
             faceTokenCompare.push(this.props.familyObject.guardian[i].face_token);
         };
-        console.log("faceTokenCompare = ", faceTokenCompare );
+        console.log("faceTokenCompare = ", faceTokenCompare);
         //check to see if the school exists and if so pull back faceSetToken
         API.getSchool({
             schoolName: this.state.schoolName
         })
             .then(schoolresult => {
-                console.log("School Exists ",schoolresult.data[0].faceSetToken)
+                console.log("School Exists ", schoolresult.data[0].faceSetToken)
                 let faceSetToken = schoolresult.data[0].faceSetToken;
-                this.callFaceDetail(faceSetToken,faceTokenCompare )
+                this.callFaceDetail(faceSetToken, faceTokenCompare)
             })
             .catch(err => this.setState({
                 error: true,
@@ -78,7 +83,7 @@ class AddChildCard extends Component {
                 console.log("face Detail result: ", faceSetResult.data)
                 let addToFaceSet = "";
                 //loop over face tokens
-                console.log("faceTokenCompare.length = ", faceTokenCompare.length );
+                console.log("faceTokenCompare.length = ", faceTokenCompare.length);
                 for (let x = 0; x < faceTokenCompare.length; x++) {
                     console.log("entering for loop of face details");
                     console.log(faceSetResult.data.indexOf(faceTokenCompare[x]));
@@ -89,12 +94,12 @@ class AddChildCard extends Component {
                             addToFaceSet = faceTokenCompare[x];
                             console.log("faceSet to add 1: ", addToFaceSet);
                         } else {
-                            addToFaceSet += ','+ faceTokenCompare[x];
+                            addToFaceSet += ',' + faceTokenCompare[x];
                             console.log("faceSet to add +: ", addToFaceSet);
                         }
-                        
+
                         //add new list of missing faces to face setID of school
-                    } 
+                    }
                 }
                 //finally add child to db
                 this.addface(faceSetToken, addToFaceSet);
@@ -141,82 +146,140 @@ class AddChildCard extends Component {
     render() {
         return (
             <Container fluid>
-                <div className="panel panel-default">
-                    <div className="panel-body">
-                        <Row>
-                            <Col size="md-4">
+                <Grid container spacing={24} justify="space-between">
+
+                    <Grid item xs={4}>
+                        <Grid container direction="column" justify="space-between" alignItems="flex-start">
+                            <Grid item style={{ marginTop: 50 }}>
                                 <AddImageCard
                                     image_base64={this.state.image_base64}
                                 />
+                                <input
+                                    type="file"
+                                    style={{ display: 'none' }}
+                                    onChange={this.handleFileSelection}
+                                    ref={fileInput => this.fileInput = fileInput}
+                                />
+                            </Grid>
 
-                                <div className="form-group">
-                                    <input
-                                        type="file"
-                                        style={{ display: 'none' }}
-                                        onChange={this.handleFileSelection}
-                                        ref={fileInput => this.fileInput = fileInput}
-                                    />
-                                </div>
-                                <FormBtn
-                                    onClick={() => this.fileInput.click()}
-                                >
+                            <Grid item >
+                                <FormBtn onClick={() => this.fileInput.click()} style={{ marginTop: 50 }}>
                                     Select Image
-                        </FormBtn>
-                            </Col>
-                            <Col size="md-6">
-                                <div className="form-group">
-                                    <Input className="form-control"
-                                        value={this.state.fName}
-                                        onChange={this.handleInputChange}
-                                        name="fName"
-                                        placeholder="Enter First Name"
-                                    />
-                                    <Input className="form-control"
-                                        value={this.state.lName}
-                                        onChange={this.handleInputChange}
-                                        name="lName"
-                                        placeholder="Enter Last Name"
-                                    />
-                                    <Input className="form-control"
-                                        value={this.state.schoolName}
-                                        onChange={this.handleInputChange}
-                                        name="schoolName"
-                                        placeholder="Enter School Name"
-                                    />
-                                    <Input className="form-control"
-                                        value={this.state.grade}
-                                        onChange={this.handleInputChange}
-                                        name="grade"
-                                        placeholder="Enter Current Grade"
-                                    />
-                                    <Input className="form-control"
-                                        value={this.state.email}
-                                        onChange={this.handleInputChange}
-                                        name="email"
-                                        type="email"
-                                        placeholder="Enter email"
-                                    />
-                                    <Input className="form-control"
-                                        value={this.state.phone}
-                                        onChange={this.handleInputChange}
-                                        name="phone"
-                                        pattern="/\d{3}-\d{3}-\d{4}/"
-                                        placeholder="Enter Phone Number"
-                                    />
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col size="md-6">
-                                <FormBtn
-                                    onClick={this.handleAddChild}
-                                >
-                                    Add Child
+                                    </FormBtn>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                    <Grid item xs={7}>
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <Person />
+                            </Grid>
+                            <Grid item sm={10}>
+                                <Input
+                                    fullWidth
+                                    value={this.state.fName}
+                                    onChange={this.handleInputChange}
+                                    name="fName"
+                                    label="Enter First Name"
+                                    placeholder="First Name"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <Person />
+                            </Grid>
+                            <Grid item sm={10}>
+                                <Input
+                                    fullWidth
+                                    value={this.state.lName}
+                                    onChange={this.handleInputChange}
+                                    name="lName"
+                                    label="Enter Last Name"
+                                    placeholder="Last Name"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <Security />
+                            </Grid>
+                            <Grid item sm={10}>
+                                <Input
+                                    fullWidth
+                                    value={this.state.schoolName}
+                                    onChange={this.handleInputChange}
+                                    name="schoolName"
+                                    label="Enter School Name"
+                                    placeholder="School Name"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <LocalLibrary />
+                            </Grid>
+                            <Grid item sm={10}>
+                                <Input
+                                    fullWidth
+                                    value={this.state.grade}
+                                    onChange={this.handleInputChange}
+                                    name="grade"
+                                    label="Enter Current Grade"
+                                    placeholder="Grade"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <Email />
+                            </Grid>
+                            <Grid item sm={10}>
+                                <Input
+                                    fullWidth
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}
+                                    name="email"
+                                    type="email"
+                                    label="Enter Email"
+                                    placeholder="Email"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container spacing={8} alignItems="flex-end">
+                            <Grid item>
+                                <Phone />
+                            </Grid>
+                            <Grid item sm={10}>
+                                <Input
+                                    fullWidth
+                                    value={this.state.phone}
+                                    onChange={this.handleInputChange}
+                                    name="phone"
+                                    pattern="/\d{3}-\d{3}-\d{4}/"
+                                    type="number"
+                                    label="Enter Phone Number"
+                                    placeholder="Phone Number"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid item sm={10}>
+                            <FormBtn
+                                onClick={this.handleAddChild}
+                                style={{ marginTop: 20, marginBottom: 30 }}>
+
+                                Add Child
                                 </FormBtn>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Container>
         );
     }
